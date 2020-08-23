f4f2be5e502f6c416e45e72494b4cb9df92a6629
wangzhi <15713598138@sina.cn>
2020-08-23 11:01:06 +0800

    28天： 第三遍提交二叉树的最大深度

--- OneQuestionPerDay/src/com/wz/time3/a21/MaximumDepth.java
+++ OneQuestionPerDay/src/com/wz/time3/a21/MaximumDepth.java
@@ -25,14 +25,52 @@ public class MaximumDepth {
      * 想法对着，可是我一直想着人肉递归，然后就绕进去了，就懵逼了
      */
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode root1 = new TreeNode(9);
+        TreeNode root2 = new TreeNode(02);
+        TreeNode root3 = new TreeNode(15);
+        TreeNode root4 = new TreeNode(7);
+        root.left = root1;
+        root.right = root2;
+        root2.left = root3;
+        root2.right = root4;
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
     /**
      * 时间复杂度O(n)
      * 空间复杂度O(深度) 递归函数需要栈空间
      */
-    public static int maxDepth(TreeNode root) {
-        return 0;
+    /*public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return getResult(root, 1);
 
     }
+
+    private static int getResult(TreeNode root, int num) {
+        if (root.left == null && root.right == null) {
+            return num;
+        }
+        int left = 0;
+        int right = 0;
+        if (root.left != null) {
+            left = getResult(root.left, num + 1);
+        }
+        if (root.right != null) {
+            right = getResult(root.right, num + 1);
+        }
+        return  Math.max(left, right);
+    }*/
 }
 
 
