2861c4e99c9ee3ce6e57df7fcc7d9d81918da2bb
wzlove <2585700076@qq.com>
2020-08-24 19:36:36 +0800

    29. 第二遍提交买卖股票的最佳时机

--- Week_04/homework/src/time2/a5/BestTimeToBuyAndSellStocks.java
+++ Week_04/homework/src/time2/a5/BestTimeToBuyAndSellStocks.java
@@ -52,7 +52,12 @@ public class BestTimeToBuyAndSellStocks {
 
     public static int maxProfit(int[] prices) {
         int result = 0;
-        
+        for (int i = 0; i < prices.length - 1; i++) {
+            int temp = prices[i + 1] - prices[i];
+            if (temp > 0) {
+                result += temp;
+            }
+        }
         return result;
     }
 }
