85b2bc62a7913c05d3aaf60a539c4772fc513178
wangzhi <15713598138@sina.cn>
2020-08-28 19:51:20 +0800

    第二遍提交 最大子序和

--- OneQuestionPerDay/src/com/wz/august/time2/a27/MaximumSubsequenceSum.java
+++ OneQuestionPerDay/src/com/wz/august/time2/a27/MaximumSubsequenceSum.java
@@ -30,7 +30,14 @@ public class MaximumSubsequenceSum {
      * 空间复杂度为O(1)
      */
     public static int maxSubArray(int[] nums) {
-
-        return -1;
+        // 最大值
+        int max = nums[0];
+        // 前一位
+        int pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            max = Math.max(pre, max);
+        }
+        return max;
     }
 }
